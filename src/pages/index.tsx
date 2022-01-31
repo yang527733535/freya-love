28b8b7ea4987974d83060aced6780c95ec6e9c22
MRHHH <15817257525@163.com>
2022-01-31 21:26:34 +0800

    🐴增加新功能

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.less';
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>新年快乐呀</h1>
+      <h1 className={styles.title}>🐅虎年快乐呀,新年快乐呀</h1>
     </div>
   );
 }
